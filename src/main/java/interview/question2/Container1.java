3ed27f778ff903c05ab5e4921630d88ed9067c57
sunpeihe <sunpeihe@inspur.com>
2020-10-16 23:24:59 +0800

    两个练习面试题调整优化

--- src/main/java/interview/question2/Container1.java
+++ src/main/java/interview/question2/Container1.java
@@ -5,11 +5,10 @@ import java.util.LinkedList;
 public class Container1<T> {
     final private LinkedList<T> lists = new LinkedList<>();
     final private int MAX=10;
-    //volatile int count=0;
     public synchronized void put(T o){
         while (lists.size()==MAX){
             try {
-                System.out.println("生产完毕，生产者阻塞"+Thread.currentThread().getName());
+                System.out.println("生产完毕，生产者 "+Thread.currentThread().getName()+" 阻塞中");
                 this.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -18,13 +17,12 @@ public class Container1<T> {
 
         lists.add(o);
         this.notifyAll();
-        //count++;
     }
 
     public synchronized T get(){
         while (lists.size()==0){
             try {
-                System.out.println("消耗完毕，消费者阻塞"+Thread.currentThread().getName());
+                System.out.println("库存为0，消费者 "+Thread.currentThread().getName()+" 阻塞中");
                 this.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -46,7 +44,7 @@ public class Container1<T> {
         for (int i = 0; i <10 ; i++) {
             new Thread(()->{
                 for (int j = 0; j < 5; j++) {//每个消费者线程消耗5个对象
-                    System.out.println(Thread.currentThread().getName()+" 消费了 "+c.get());
+                    System.out.println(Thread.currentThread().getName()+" 消费了 "+c.get()+" 库存剩余："+c.getCount());
                 }
             },"consumer"+i).start();
         }
@@ -56,7 +54,7 @@ public class Container1<T> {
             new Thread(()->{
                 for (int j = 0; j <25 ; j++) {//每个生产者线程生产25个对象
                     c.put(Thread.currentThread().getName()+" "+j);
-                    System.out.println(Thread.currentThread().getName()+" 生产了 "+Thread.currentThread().getName()+" "+j);
+                    System.out.println(Thread.currentThread().getName()+" 生产了 "+Thread.currentThread().getName()+" "+j+" 库存剩余："+c.getCount());
                 }
             },"producer"+i).start();
         }
--- src/main/java/interview/question2/Container2.java
+++ src/main/java/interview/question2/Container2.java
@@ -1,6 +1,7 @@
 package interview.question2;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,7 +12,6 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Container2<T> {
     final private LinkedList<T> lists = new LinkedList<>();
     final private int MAX = 10;
-    //volatile int count=0;
     ReentrantLock lock = new ReentrantLock();
     Condition consumer = lock.newCondition();
     Condition producer = lock.newCondition();
@@ -20,12 +20,13 @@ public class Container2<T> {
         try {
             lock.lock();
             while (lists.size() == MAX) {
-                System.out.println("生产完毕，生产者阻塞" + Thread.currentThread().getName());
+                System.out.println("生产完毕，生产者 "+Thread.currentThread().getName()+" 阻塞中");
                 producer.await();
             }
 
             lists.add(o);
             consumer.signalAll();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -33,20 +34,22 @@ public class Container2<T> {
         }
     }
 
-    public synchronized T get() {
+    public T get() {
         T t=null;
+        lock.lock();
         try {
-            lock.lock();
             while (lists.size() == 0) {
-                System.out.println("消耗完毕，消费者阻塞" + Thread.currentThread().getName());
+                System.out.println("库存为0，消费者 "+Thread.currentThread().getName()+" 阻塞中");
                 consumer.await();
             }
+
             t = lists.removeFirst();
             producer.signalAll();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();
+           lock.unlock();
         }
         return t;
     }
@@ -56,23 +59,24 @@ public class Container2<T> {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Container2 c = new Container2();
 
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
                 for (int j = 0; j < 5; j++) {//每个消费者线程消耗5个对象
-                    System.out.println(Thread.currentThread().getName() + " 消费了 " + c.get());
+                    System.out.println(Thread.currentThread().getName()+" 消费了 "+c.get()+" 库存剩余："+c.getCount());
                 }
             }, "consumer" + i).start();
         }
 
+        TimeUnit.SECONDS.sleep(5);
 
         for (int i = 0; i < 2; i++) {
             new Thread(() -> {
                 for (int j = 0; j < 25; j++) {//每个生产者线程生产25个对象
                     c.put(Thread.currentThread().getName() + " " + j);
-                    System.out.println(Thread.currentThread().getName() + " 生产了 " + Thread.currentThread().getName() + " " + j);
+                    System.out.println(Thread.currentThread().getName()+" 生产了 "+Thread.currentThread().getName()+" "+j+" 库存剩余："+c.getCount());
                 }
             }, "producer" + i).start();
         }
